43aeddda72de53d184c8af05481f9c81a84794f9
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2021-07-05 06:03:25 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## A simple web scrapper written in V ([V Language](https://vlang.io)). It returns a JSON list of Ghana's members of parliament along with their pictures.
+## This is me exploring what the V language has to offer
+
+
+The program works. You can use it to retrieve a list of Ghana's members of parliamant.
+After which you can use the JSON in your project or however you wish.
+
+Drop me a comment if you find this useful:
+> Twitter: [@yeboahnanaosei](https://twitter.com/yeboahnanaosei)
+
+> Email: [yeboahnanaosei@gmail.com](mailto://yeboahnanaosei@gmail.com)
---
+++ main.v
@@ -0,0 +1,80 @@
+import net.http
+import net.html
+import json
+
+struct Member {
+	name         string
+	party        string
+	party_abbr   string
+	constituency string
+	region       string
+	photo        string
+}
+
+const base_url = 'https://www.parliament.gh'
+
+fn get_page_links(base_url string) ?[]string {
+	res := http.get(base_url) ?
+	document := html.parse(res.text)
+	anchor_tags := document.get_tag('a')
+
+	mut page_links := []string{}
+	for tag in anchor_tags {
+		if 'class' in tag.attributes && tag.attributes['class'].contains('square') {
+			page_links << tag.attributes['href']
+		}
+	}
+	return page_links
+}
+
+fn get_members(url string) []Member {
+	res := http.get(url) or { return []Member{} }
+	document := html.parse(res.text)
+	mp_divs := document.get_tag('div')
+	mut members := []Member{}
+
+	for div in mp_divs {
+		// We are looking for divs that have the attribute 'class' set to a value
+		if 'class' !in div.attributes {
+			continue
+		}
+
+		// If the class attribute is set above then we are looking for class
+		// that contain the word 'mpcard'
+		if !div.attributes['class'].contains('mpcard') {
+			continue
+		}
+
+		party_details := div.children[0].children[1].children[1].children[1].content.split('(')
+		members << Member{
+			name: div.children[0].children[1].children[1].children[0].content.split(' ').filter(it.trim_space().len > 0).map(it.to_upper()).join(' ')
+			party: party_details[0].trim_space().to_upper()
+			party_abbr: party_details[1].trim(' )(').to_upper()
+			constituency: div.children[0].children[1].children[1].children[3].content.to_upper()
+			region: div.children[0].children[1].children[1].children[5].content.to_upper()
+			photo: '$base_url/${div.children[0].children[0].attributes['src']}'
+		}
+	}
+	return members
+}
+
+fn main() {
+	page_links := get_page_links(base_url+"/mps?az") or {
+		println('cannot continue: $err')
+		return
+	}
+
+	mut threads := []thread []Member{}
+	for link in page_links {
+		threads << go get_members('$base_url/$link')
+	}
+	members := threads.wait()
+
+	mut final_data := []Member{cap: 275}
+	for arr in members {
+		for mp in arr {
+			final_data << mp
+		}
+	}
+	println(json.encode(final_data))
+}
